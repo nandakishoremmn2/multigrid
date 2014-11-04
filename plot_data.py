2f1464d8dd9bf42232e196af0dd993c9645371d3
nandu <nandakishoremmn@gmail.com>
2014-11-05 04:55:12 +0530

    added smoothening factor plot

--- plot_data.py
+++ plot_data.py
@@ -12,8 +12,29 @@ from pylab import *
 
 data = loadtxt('data.dat' if len(sys.argv) < 2 else sys.argv[1])
 
-plot(cumsum(data[:,2]), data[:,1], '-o')
+
+wd = data[:,2]
+wu = cumsum(data[:,2])
+res = data[:,1]
+mask = (data[:,3] == 0)
+plot(wu, res, '-o', wu[mask], res[mask], '-o')
 yscale('log')
 axis('image')
+title('Plot of residue')
+xlabel('Work Units')
+ylabel('norm-2 of residue')
+legend(['residue', 'residue of fine grid alone'])
+
+show()
+
+mask = mask * (wd != 0)
+
+wdm = wd[mask]
+wum = wu[mask]
+resm = res[mask]
+plot(wum[1:], pow(resm[1:]/resm[:-1], 1/wdm[1:]), '-o')
+title('Smoothing Factor of residue (avgeraged)')
+xlabel('Work Units')
+ylabel('Smoothing Factor')
 
 show()
